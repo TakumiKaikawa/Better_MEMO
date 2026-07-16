a2116a2e1023339a122e2bdc5fca743a08c1f22a
TakumiKaikawa <kannai1585hama@gmail.com>
2026-07-16 20:05:25 +0900

    Arraylist listを作成

--- Better_MEMO.java
+++ Better_MEMO.java
@@ -3,6 +3,9 @@ import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
 import javafx.scene.control.Label;
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 
 public class Better_MEMO extends Application{
@@ -12,10 +15,21 @@ public class Better_MEMO extends Application{
 
     public void start(Stage stage){
         VBox layout = new VBox(20);
+        layout.setAlignment(Pos.CENTER);
 
-        Scene scene = new Scene(layout,600,300);
+        Scene scene = new Scene(layout,300,600);
         Label titleLabel = new Label("Better_MEMO");
         layout.getChildren().add(titleLabel);
+
+        TextField textfield = new TextField();
+        layout.getChildren().add(textfield);
+
+        Button btn = new Button("記録する");
+        layout.getChildren().add(btn);
+
+        btn.setOnAction(e ->{
+            String record = textfield.getText();
+        });
 
         stage.setScene(scene);
         stage.show();
--- memory.java
+++ memory.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+
 public class memory{
-    
+
+    ArrayList<String> list = new ArrayList<>();
 }
